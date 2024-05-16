620c9c4d61a18cab9d3c5ad4e5744098edcd1102
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-05-16 13:52:59 +0330

    fixed clock add momentjs

--- stories/RahsazAdmin/Clock/Clock.tsx
+++ stories/RahsazAdmin/Clock/Clock.tsx
@@ -28,26 +28,21 @@ export const Clock = () => {
                 second: "2-digit"
             }).format(time)}
             color="foreground"
+            placement="left"
         >
             <div
                 className="h-12 w-12 select-none transition duration-1000 hover:scale-150 active:scale-90 bg-white flex justify-center items-center rounded-full relative shadow-2xl text-lg text-gray-500 text-center after:bg-gray-300 after:w-1 after:h-1 after:rounded-full after:absolute after:z-10 after:border-0.25 after:border-white">
                 <div
                     className="absolute rounded w-[2px] h-2.5 bg-black origin-bottom top-3.5"
-                    style={{
-                        transform: `rotateZ(${time.getHours() * 30}deg)`
-                    }}
+                    style={{transform: `rotateZ(${time.getHours() * 30}deg)`}}
                 />
                 <div
                     className="absolute rounded w-[2px] h-3 bg-gray-400 origin-bottom top-3"
-                    style={{
-                        transform: `rotateZ(${time.getMinutes() * 6}deg)`
-                    }}
+                    style={{transform: `rotateZ(${time.getMinutes() * 6}deg)`}}
                 />
                 <div
                     className="absolute rounded w-[2px] h-4 bg-red-600 origin-bottom top-2"
-                    style={{
-                        transform: `rotateZ(${time.getSeconds() * 6}deg)`
-                    }}
+                    style={{transform: `rotateZ(${time.getSeconds() * 6}deg)`}}
                 />
                 <div className="absolute w-full h-full z-20">
                     <div className="relative w-full h-full flex justify-center items-center text-[0.35rem] leading-none text-gray-800 font-black">
