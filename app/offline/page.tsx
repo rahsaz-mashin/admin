b0cd67777d0eb852f451c8718fcc7d40dd66dbae
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-05-16 21:07:38 +0330

    pwa fixed offline

---
+++ app/offline/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <span className="text-primary font-bold text-xl">شما آفلاین هستید!</span>
+        </div>
+    );
+}
--- next.config.js
+++ next.config.js
@@ -8,7 +8,16 @@ const withPWA = require('next-pwa')({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    runtimeCaching: require("next-pwa/cache")
+    runtimeCaching: require("next-pwa/cache"),
+    cacheOnFrontEndNav: true,
+    reloadOnOnline: true,
+    fallbacks: {
+        //image: "/static/images/fallback.png",
+        document: "/offline",
+        // font: '/static/font/fallback.woff2',
+        // audio: ...,
+        // video: ...,
+    },
 })
 
 module.exports = withPWA(nextConfig)
