90a0e0f70f4a216f573d85ff4d685beaf0f3242e
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-06-29 14:05:20 +0330

    fixed product page

--- stories/RahsazStore/Product/RelatedProductsBox/RelatedProductsBox.tsx
+++ stories/RahsazStore/Product/RelatedProductsBox/RelatedProductsBox.tsx
@@ -13,33 +13,6 @@ export const ProductRelatedProductsBox = (
         ProductRelatedProductsBoxProps
 ) => {
 
-    const items = [
-        {
-            title: "پرداخت امن",
-            description: "با تمامی کارت های عضو شبکه شتاب",
-            icon: CardPaymentIcon
-        },
-        {
-            title: "کیف پول",
-            description: "امکان پرداخت با چک، پایا، اقساط و ...",
-            icon: WalletIcon
-        },
-        {
-            title: "ارسال سریع",
-            description: "ارسال کالا با روش های مختلف و در کمترین زمان ممکن",
-            icon: FastDeliveryIcon
-        },
-        {
-            title: "پشتیبانی",
-            description: "7 روز هفته، 24 ساعت شبانه روز",
-            icon: SupportIcon
-        },
-        {
-            title: "بازگشت کالا",
-            description: "ضمانت 7 روز بازگشت کالا در صورت مغایرت",
-            icon: ReverseProductIcon
-        },
-    ]
 
 
     return (
@@ -48,25 +21,7 @@ export const ProductRelatedProductsBox = (
                 <h3 className="font-bold text-lg">کالاهای مشابه</h3>
             </CardHeader>
             <CardBody className="text-start py-3 flex-col">
-                <div className="flex items-center gap-4 overflow-y-hidden px-0.5 hide-scrollbar">
-                    {items.map((v, i) => {
-                        return (
-                            <div key={i} className="flex-shrink-0 relative flex items-center ps-5">
-                                <div className="absolute h-10 w-10 text-primary -start-0 z-20">
-                                    <v.icon/>
-                                </div>
-                                <Card shadow="none" className="bg-zinc-300">
-                                    <CardHeader className="font-bold text-sm text-green-600 pb-1 ps-6">
-                                        {v.title}
-                                    </CardHeader>
-                                    <CardBody className="font-light text-xs text-gray-600 pt-1 ps-6">
-                                        {v.description}
-                                    </CardBody>
-                                </Card>
-                            </div>
-                        )
-                    })}
-                </div>
+
             </CardBody>
         </Card>
     );
