4ff79a98789a91431a2357357354560121e2415a
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-07-19 21:51:05 +0330

    pages (dashboard, wallet, orders, order) added

---
+++ app/(store)/dashboard/wallet/page.tsx
@@ -0,0 +1,35 @@
+import {CartPendingOrders} from "@/stories/RahsazStore/Cart/PendingOrders/PendingOrders";
+import {CartTabs} from "@/stories/RahsazStore/Cart/Tabs";
+import {CartDeliveryTypeBox} from "@/stories/RahsazStore/Cart/DeliveryTypeBox";
+import React from "react";
+import {CartAddressBox} from "@/stories/RahsazStore/Cart/AddressBox";
+import {CartPaymentWayBox} from "@/stories/RahsazStore/Cart/PaymentWayBox";
+import {CartCouponBox} from "@/stories/RahsazStore/Cart/CouponBox";
+import {CartSummaryBox} from "@/stories/RahsazStore/Cart/SummaryBox";
+import {CartPreInvoice} from "@/stories/RahsazStore/Cart/PreInvoice";
+import {CartSubmitBox} from "@/stories/RahsazStore/Cart/SubmitBox";
+
+export default function Page() {
+    return (
+        <main className="grid grid-cols-6 p-4 gap-4">
+            کیف پول
+            {/*<section className="col-span-full lg:col-span-4 relative">*/}
+            {/*    <div className="flex flex-col gap-3 relative">*/}
+            {/*        <CartPendingOrders/>*/}
+            {/*        <CartTabs/>*/}
+            {/*    </div>*/}
+            {/*</section>*/}
+            {/*<section className="col-span-full lg:col-span-2 relative">*/}
+            {/*    <div className="flex flex-col gap-3 relative">*/}
+            {/*        <CartSummaryBox/>*/}
+            {/*        <CartPreInvoice/>*/}
+            {/*        <CartDeliveryTypeBox/>*/}
+            {/*        <CartAddressBox/>*/}
+            {/*        <CartPaymentWayBox/>*/}
+            {/*        <CartCouponBox/>*/}
+            {/*    </div>*/}
+            {/*    <CartSubmitBox/>*/}
+            {/*</section>*/}
+        </main>
+    );
+}
