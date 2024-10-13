e6e12c44af377d6a16b4ed21d29b81f30e1aa4fb
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-10-13 19:41:06 +0330

    product List fixed

--- components/admin/productList.tsx
+++ components/admin/productList.tsx
@@ -1,5 +1,8 @@
 import {Product} from "@/interfaces/Product.interface";
 import {ColumnType} from "@/stories/RahsazAdmin/TableList";
+import {Chip} from "@nextui-org/chip";
+import {ProductCategory} from "@/interfaces/ProductCategory.interface";
+import {ProductMachineModel} from "@/interfaces/ProductMachineModel.interface";
 
 
 type T = Product
@@ -30,7 +33,60 @@ const tableColumns: ColumnType<T>[] = [
     {
         key: "title",
         title: "عنوان",
-        minWidth: 160,
+        minWidth: 280,
+        render: (value, ctx) => {
+            return (
+                <div className="flex flex-col gap-2">
+                    <span>{value}</span>
+                    <div className="flex flex-row gap-2 items-center">
+                        <span className="font-medium">دیگر نام ها:</span>
+                        {ctx?.names?.map((v, idx) => {
+                            return (
+                                <Chip key={idx} size="sm" variant="flat" color="secondary">
+                                    {v}
+                                </Chip>
+                            )
+                        })}
+                    </div>
+                </div>
+            )
+        },
+    },
+    {
+        key: "categories",
+        title: "دسته بندی",
+        minWidth: 240,
+        render: (value: ProductCategory[], ctx) => {
+            return (
+                <div className="flex gap-2 items-center">
+                    {value?.map((v, idx) => {
+                        return (
+                            <Chip key={v.id} size="md" variant="shadow" color="primary">
+                                {v.title}
+                            </Chip>
+                        )
+                    })}
+                </div>
+            )
+        },
+    },
+    {
+        key: "machinery",
+        title: "ماشین آلات",
+        minWidth: 240,
+        render: (value: ProductMachineModel[], ctx) => {
+            return (
+                <div className="flex gap-2 items-center">
+                    {value?.map((v, idx) => {
+                        return (
+                            <Chip key={v.id} size="md" variant="shadow" color="primary">
+                                {v.title}
+                            </Chip>
+                        )
+                    })}
+                </div>
+            )
+        },
     },
 ]
 
