2fa2e8045b07c659fe40af367b5a15a41fb98377
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-10-25 17:37:54 +0330

    working on admin users

--- stories/RahsazStore/Product/IntroBox/IntroBox.tsx
+++ stories/RahsazStore/Product/IntroBox/IntroBox.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, CardBody, CardHeader, CardFooter} from "@nextui-org/card";
+import {Card, CardBody, CardHeader} from "@nextui-org/card";
 import Link from "next/link";
 import {ScrollShadow} from "@nextui-org/react";
 
@@ -20,13 +20,13 @@ export const ProductIntroBox = (props: ProductIntroBoxProps) => {
     return (
         <div className="pt-4 px-4" id="intro">
             <Card shadow="none" className="bg-gray-100">
-                <Link href="#intro" className="outline-none">
+                <Link href={{hash: "intro"}} className="outline-none">
                     <CardHeader className="flex justify-between py-2">
                         <h3 className="font-bold text-lg">معرفی کلی</h3>
                     </CardHeader>
                 </Link>
                 <CardBody className="text-start py-3 flex-col">
-                    <ScrollShadow hideScrollBar className="h-96">
+                    <ScrollShadow hideScrollBar className="max-h-96">
                         <div className="text-justify" dangerouslySetInnerHTML={{__html: intro}}/>
                     </ScrollShadow>
                 </CardBody>
