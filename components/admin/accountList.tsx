c772d1029dabfc382c32db415ff5aad9b8f9d6b7
rasoul707 <r.ahmadifar.1377@gmail.com>
2024-11-09 14:03:09 +0330

    working on identity

--- components/admin/accountList.tsx
+++ components/admin/accountList.tsx
@@ -242,7 +242,6 @@ const tableColumns: ColumnType<T>[] = [
                             ثبت نشده
                         </Chip>
                     )}
-
                     {!!value && (
                         <div className="flex flex-col gap-3 items-start justify-center">
                             <div className="flex flex-row gap-2 items-center">
